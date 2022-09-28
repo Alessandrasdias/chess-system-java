923bb8071ddc592f084626696fb69a78b760c473
Alessandra Santos Dias <alessandrasdiastrabalho@gmail.com>
2022-09-28 16:40:50 -0300

    Possible Moves

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,7 +1,7 @@
 package boardgame;
 
 //is associated with Board
-public class Piece {
+public abstract class Piece {
 	//Shouldn't be visible bc is abstract
 	protected Position position;
 	private Board board;
@@ -15,6 +15,24 @@ public class Piece {
 	//Only classes inside the package and subclasses can access board
 	protected Board getBoard() {
 		return board;
+	}
+	
+	public abstract boolean[][] possibleMoves();
+	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 }
